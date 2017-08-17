967d339017b2c5997ca94060045dd233f11c6d51
Frew A Negash <zulufanke@yahoo.com>
2017-08-17 12:16:22 -0400

    wdi-radio-lab submitted

---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict'
+
+angular
+ .module('wdi_radio', [
+   'ui.router',
+   'ngResource'
+ ])
+ .config([
+   '$stateProvider',
+   '$urlRouterProvider',
+   RouterFunction
+ ])
+.factory('Songs', [
+  '$resource',
+  songsService
+])
+.controller('SongsIndexController', [
+  'Songs',
+  SongsIndexControllerFunction
+])
+function SongsIndexControllerFunction (Songs) {
+  this.songs = Songs.query()
+}
+
+function songsService ($resource) {
+  return $resource('http://localhost:3000/songs.json', {}, {
+    update: {
+      method: 'PUT'}
+  })
+}
+
+function RouterFunction ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('homepage', {
+    url: '/',
+    templateUrl: 'js/ng-views/homepage.html'
+  })
+   .state('SongsIndex', {
+     url: '/songs',
+     templateUrl: 'js/ng-views/index.html',
+     controller: 'SongsIndexController',
+     controllerAs: 'vm'
+   })
+  $urlRouterProvider.otherwise('/')
+}
